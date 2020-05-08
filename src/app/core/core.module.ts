fd9bcba21431177465605ac1abd8d9438cadfae1
ravirrr <rutvij.ravi@rabobank.nl>
2020-05-08 11:06:39 +0530

    Completed initial app header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule.forChild([])],
+  exports: [RouterModule, MaterialModule, HeaderComponent],
 })
-export class CoreModule {}
+export class CoreModule { }
